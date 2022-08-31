8d1f9bb32d020a6237b9272491dfde23083f365b
tencho <tencho@gmail.com>
2022-08-31 17:30:52 -0400

    Swagger Agregado

--- UsuarioApp.WebApi/Program.cs
+++ UsuarioApp.WebApi/Program.cs
@@ -10,12 +10,20 @@ builder.Services.AddSqlServer<UsuarioApp.Modelo.UsuariosAppContext>(builder.Conf
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IServicioRol, ServicioRol>();
 builder.Services.AddScoped<IServicioUsuario, ServicioUsuario>();
-
 builder.Services.AddControllers();
 
+builder.Services.AddOpenApiDocument(options => {
+    options.Version = "v0.0.1";
+    options.Title = "App Usuario";
+    options.Description = "EndPoints de la aplicacion";
+});
 builder.Services.AddAutoMapper(typeof(UsuarioApp.Repositorio.AutoMapper.AutoMapperProfiles).Assembly);
 
 var app = builder.Build();
+
+app.UseOpenApi(); // serve OpenAPI/Swagger documents
+app.UseSwaggerUi3(); // serve Swagger UI
+app.UseReDoc(); // serve ReDoc UI
 
 // Configure the HTTP request pipeline.
 

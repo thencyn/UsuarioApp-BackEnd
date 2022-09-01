046bffa1676fa54d4b61ee3a78fc1d8069eb1a61
tencho <tencho@gmail.com>
2022-09-01 11:59:20 -0400

    DataAnnotations Agregado

--- UsuarioApp.Comun/DTO/RolDTO.cs
+++ UsuarioApp.Comun/DTO/RolDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ namespace UsuarioApp.Comun.DTO
     public class RolDTO
     {
         public int? IdRol { get; set; }
+
+        [MaxLength(30, ErrorMessage = "El largo máximo es de 30 caracteres")]
+        [MinLength(3, ErrorMessage = "El largo mínimo es de 3 caracteres")]
         public string Nombre { get; set; }
         public bool RegistroVigente { get; set; }
     }
--- UsuarioApp.Comun/Mensajes/Usuario/UsuarioCrearRequerimiento.cs
+++ UsuarioApp.Comun/Mensajes/Usuario/UsuarioCrearRequerimiento.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,19 @@ namespace UsuarioApp.Comun.Mensajes.Usuario
 {
     public class UsuarioCrearRequerimiento
     {
+        [EmailAddress(ErrorMessage = "El email no es valido")]
         public string Correo { get; set; }
+        
+        [MaxLength(30, ErrorMessage = "El largo máximo es de 30 caracteres")]
+        [MinLength(3, ErrorMessage = "El largo mínimo es de 3 carácteres")]
         public string Nombre { get; set; }
+
+        [MaxLength(12, ErrorMessage = "El largo máximo es de 12 caracteres")]
+        [MinLength(6, ErrorMessage = "El largo mínimo es de 6 carácteres")]
         public string Password { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "El IdRol debe ser mayor a 1")]
         public int IdRol { get; set; }
     }
 }
